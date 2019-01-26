c16713c65c3356868fc4533a1d7bcda8efa61892
T. Matthaei <t.matthaei@gmail.com>
2019-01-26 21:32:26 +0100

    getter

--- src/offset_ptr.hpp
+++ src/offset_ptr.hpp
@@ -1,17 +1,73 @@
+#if !defined(OFFSET_PTR)
+#define OFFSET_PTR
+
+#include <vector>
+#include <stdio.h>
+
 namespace optr
 {
   template<typename T>
   class offset_ptr
   {
   public:
-    offset_ptr(T* base_, size_t offset_):
-      base(base_),
-      offset(offset_){
+    offset_ptr(T* ptr, size_t offset)
+    {
+      static_assert(1 == sizeof(uint8_t));
+      
+      byte_offset = offset / byte_size;
+      bit_offset = offset % byte_size;
+      base = reinterpret_cast<T*>(reinterpret_cast<uint8_t*>(ptr) + byte_offset);
+      make_copy();
+    }
 
+    T get(){
+      return copy;
     }
   protected:
     T* base;
     size_t offset;
+  private:
+    static constexpr unsigned int byte_size = 8;
+    size_t byte_offset;
+    size_t bit_offset;
+    T copy;
+    //std::vector<uint8_t> copy;
+
+    void make_copy(){
+      printf("base: %d\n", *base);
+      printf("size: %lu\n", sizeof(T)+1);
+      std::vector<uint8_t> data_copy (reinterpret_cast<uint8_t*>(base), reinterpret_cast<uint8_t*>(base) + sizeof(T)+1);
+      printf("data_copy: %d (size: %lu)\n", *(T*)data_copy.data(), data_copy.size());
+      
+      //Assumes that there actually is data to extract.
+      static_assert(sizeof(T) > 0, "Cannot extract data from an empty data structure.");
+
+      const uint8_t full_byte = 0xFF;
+
+      //first bit_offset bits of uint8_t
+      const uint8_t left = full_byte << (byte_size - bit_offset);
+      //all other bits
+      const uint8_t right = full_byte >> bit_offset;
+
+      //Anything within offset is uninteresting.
+      data_copy[0] &= right;
+
+      for(size_t i = 0; i < sizeof(T); i++){
+        //Part that is already in the correct byte.
+        data_copy[i] = (data_copy[i] & right) << bit_offset;
+        //Part from the next byte that gets transferred.
+        data_copy[i] += (data_copy[i+1] & left) >> (byte_size - bit_offset);
+        printf("data_copy[%2lu] - %#02x\n", i, data_copy[i]);
+      }
+
+      //Truncate last bit.
+      data_copy.resize(sizeof(T));
+
+      copy = *reinterpret_cast<T*>(data_copy.data());
+    }
   };
     
 } // optr
+
+#endif // OFFSET_PTR
+
--- test/offset_ptr_test.cpp
+++ test/offset_ptr_test.cpp
@@ -2,7 +2,25 @@
 #include "src/offset_ptr.hpp"
 
 using namespace optr::tests;
+
 TEST_F(OffsetPtrTest, it_instantiates){
-  int value = 10;
-  optr::offset_ptr<int> int_ptr(&value, 0);
+  uint32_t value = 10;
+  optr::offset_ptr<uint32_t> int_ptr(&value, 0);
+}
+
+TEST_F(OffsetPtrTest, it_gets_value_no_offset){
+  uint32_t value = 10;
+  optr::offset_ptr<uint32_t> int_ptr(&value, 0);
+  EXPECT_EQ(value, int_ptr.get());
+}
+
+TEST_F(OffsetPtrTest, it_gets_value_offset){
+  //Test data is 00{10 1011 10}00 0000
+  uint8_t raw[2];
+  raw [0] = 0x2B;
+  raw [1] = 0x80;
+  const size_t offset = 2;
+  const uint8_t value = 0xAE;
+  optr::offset_ptr<uint8_t> int_ptr(raw, offset);
+  EXPECT_EQ(value, int_ptr.get());
 }
