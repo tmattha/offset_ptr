a9189f336c19a397d1e27b3179bf3b62d14f0ffb
T. Matthaei <t.matthaei@gmail.com>
2019-01-26 18:34:02 +0100

    test setup

--- test/offset_ptr_test.cpp
+++ test/offset_ptr_test.cpp
@@ -1,2 +1,6 @@
 #include "offset_ptr_test.hpp"
 
+using namespace optr::tests;
+TEST_F(OffsetPtrTest, it_instantiates){
+  
+}
---
+++ test/offset_ptr_test.hpp
@@ -0,0 +1,17 @@
+#if !defined(OFFSET_PTR_TEST)
+#define OFFSET_PTR_TEST
+
+#include <gtest/gtest.h>
+
+namespace optr{
+    namespace tests
+    {
+        class OffsetPtrTest : public testing::Test{
+
+        };
+    }  // tests
+}  // optr
+
+
+
+#endif // OFFSET_PTR_TEST
