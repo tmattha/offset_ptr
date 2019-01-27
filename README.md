014ce5a4daf7c81ea6567603aeeb259198e413e9
Tilmann Matthaei <tech.matthaei@gmail.com>
2019-01-27 11:37:03 +0100

    Initial Development (#2)
    - Implemented offset_ptr with operators -> and *.
    - Added gtest test cases for offset_ptr.
    - Bazel build script.
    - Added some basic information to the README.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # offset_ptr
+
+## Usage
+offset_ptr is expected to behave just like a regular pointer. Offsetted data will be written every time `flush()` is called or when the object is destructed. Check out test/offset_ptr_test.cpp for more examples.
+
+### Example
+```cpp
+uint8_t raw[2];
+raw[0] = 0x00;
+raw[1] = 0x00;
+const size_t offset = 2;
+const uint8_t value = 0xAE; //1010 1110
+optr::offset_ptr<uint8_t> int_ptr(raw, offset);
+*int_ptr = value;
+//raw will look like this: 00{10 1011 10}00 0000 (0x2B80).
+```
+### Including
+As offset_ptr is a template class all implementation is within its .hpp file. Just copy src/offset_ptr.hpp anywhere and use it. offset_ptr requires a compiler supporting at leas the C++11 standard.
+
+## Use Case
+offset_ptr is aimed to streamline the process of reading data from and writing data to bit-shifted raw data. The most common use case would be working with highly fixed-layout bus messages (like CAN frames).
+
+## Contributing
+File an issue and submit a pull request if you want to.
---
+++ test/offset_ptr_test.cpp
@@ -0,0 +1,122 @@
+/**
+ *  @author Tilmann Matthaei <t.matthaei@gmail.com>
+ */
+
+#include "offset_ptr_test.hpp"
+#include "src/offset_ptr.hpp"
+#include "NonPrimitive.hpp"
+
+using namespace optr::tests;
+
+TEST_F(OffsetPtrTest, it_instantiates){
+  uint32_t value = 10;
+  optr::offset_ptr<uint32_t> int_ptr(&value, 0);
+}
+
+TEST_F(OffsetPtrTest, it_gets_value_no_offset){
+  uint32_t value = 10;
+  optr::offset_ptr<uint32_t> int_ptr(&value, 0);
+  EXPECT_EQ(value, *int_ptr);
+}
+
+TEST_F(OffsetPtrTest, it_gets_value_offset){
+  //Test data is 00{10 1011 10}00 0000
+  uint8_t raw[2];
+  raw[0] = 0x2B;
+  raw[1] = 0x80;
+  const size_t offset = 2;
+  const uint8_t value = 0xAE;
+  optr::offset_ptr<uint8_t> int_ptr(raw, offset);
+  EXPECT_EQ(value, *int_ptr);
+}
+
+TEST_F(OffsetPtrTest, it_gets_value_offset_greater_1_byte){
+  //Test data is 0000 0000 00{10 1011 10}00 0000
+  uint8_t raw[3];
+  raw[0] = 0;
+  raw[1] = 0x2B;
+  raw[2] = 0x80;
+  const size_t offset = 10;
+  const uint8_t value = 0xAE;
+  optr::offset_ptr<uint8_t> int_ptr(raw, offset);
+  EXPECT_EQ(value, *int_ptr);
+}
+
+
+TEST_F(OffsetPtrTest, it_sets_and_gets_the_same_no_offset){
+  uint32_t value = 10;
+  uint32_t other_value = 20;
+  optr::offset_ptr<uint32_t> int_ptr(&value, 0);
+  *int_ptr = other_value;
+  EXPECT_EQ(other_value, *int_ptr);
+}
+
+TEST_F(OffsetPtrTest, it_flushes_on_request_no_offset){
+  uint32_t value = 10;
+  uint32_t other_value = 20;
+  optr::offset_ptr<uint32_t> int_ptr(&value, 0);
+  *int_ptr = other_value;
+  ASSERT_EQ(value, 10);
+  int_ptr.flush();
+  ASSERT_EQ(value, 20);
+}
+
+TEST_F(OffsetPtrTest, it_flushes_on_request_offset){
+  //Test data is 00{10 1011 10}00 0000
+  uint8_t raw[2];
+  raw[0] = 0x00;
+  raw[1] = 0x00;
+  const size_t offset = 2;
+  const uint8_t value = 0xAE;
+  optr::offset_ptr<uint8_t> int_ptr(raw, offset);
+  *int_ptr = value;
+  int_ptr.flush();
+  EXPECT_EQ(raw[0], 0x2B);
+  EXPECT_EQ(raw[1], 0x80);
+}
+
+TEST_F(OffsetPtrTest, it_flushes_on_multi_byte_type_request_offset){
+  //Test data is 0000 {1010 0010 1011 1000} 0000 0000 0000
+  uint8_t raw[3];
+  raw[0] = 0x00;
+  raw[1] = 0x00;
+  raw[2] = 0x00;
+  const size_t offset = 4;
+  uint16_t value;
+  
+  //prevent endianess
+  *(reinterpret_cast<uint8_t*>(&value)) = 0xA2;
+  *(reinterpret_cast<uint8_t*>(&value)+1) = 0xB8;
+
+  optr::offset_ptr<uint16_t> int_ptr(reinterpret_cast<uint16_t*>(raw), offset);
+  *int_ptr = value;
+  int_ptr.flush();
+  EXPECT_EQ(raw[0], 0x0A);
+  EXPECT_EQ(raw[1], 0x2B);
+  EXPECT_EQ(raw[2], 0x80);
+}
+
+TEST_F(OffsetPtrTest, it_can_get_ref_by_asterisk_op){
+  uint32_t value = 10;
+  optr::offset_ptr<uint32_t> int_ptr(&value, 0);
+  EXPECT_EQ(*int_ptr, value);
+}
+
+TEST_F(OffsetPtrTest, it_derefs_by_arrow_op){
+  NonPrimitive john;
+  john.name = "John Doe";
+  john.age = 45;
+  optr::offset_ptr<NonPrimitive>john_ptr(&john, 0);
+  EXPECT_EQ(john_ptr->name, "John Doe");
+  EXPECT_EQ(john_ptr->age, 45);
+}
+
+TEST_F(OffsetPtrTest, it_flushes_on_destruction){
+  uint32_t value = 10;
+  uint32_t other_value = 20;
+  auto* int_ptr = new  optr::offset_ptr<uint32_t>(&value, 0);
+  **int_ptr = other_value;
+  ASSERT_EQ(value, 10);
+  delete int_ptr;
+  ASSERT_EQ(value, 20);
+}
