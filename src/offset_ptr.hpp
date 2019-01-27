cd22a3c091a89be52c9c71afa390d65fe175281d
T. Matthaei <t.matthaei@gmail.com>
2019-01-27 10:06:05 +0100

    copy -> aligned

--- src/offset_ptr.hpp
+++ src/offset_ptr.hpp
@@ -26,27 +26,27 @@ namespace optr
       left = full_byte << (byte_size - bit_offset);
       //all other bits
       right = full_byte >> bit_offset;
-      make_copy();
+      make_aligned();
     }
 
     /**
      *  @brief Writes temporary changes back to the offsetted data.
      */
     void flush(){
-      auto copy_ptr = reinterpret_cast<const uint8_t * const>(&copy);
-      const uint8_t copy_right = full_byte >> (byte_size - bit_offset);
-      const uint8_t copy_left = full_byte << (bit_offset);
+      auto aligned_ptr = reinterpret_cast<const uint8_t * const>(&aligned);
+      const uint8_t aligned_right = full_byte >> (byte_size - bit_offset);
+      const uint8_t aligned_left = full_byte << (bit_offset);
 
       *base = (*base & left);
-      *base += ((*copy_ptr & copy_left) >> (bit_offset));
+      *base += ((*aligned_ptr & aligned_left) >> (bit_offset));
       for(size_t i = 1; i < sizeof(T); i++){
-        *(base + i) = ((*(copy_ptr + (i-1)) & copy_right) << (byte_size - bit_offset));
-        *(base + i) += ((*(copy_ptr + i) & copy_left) >> (bit_offset));
+        *(base + i) = ((*(aligned_ptr + (i-1)) & aligned_right) << (byte_size - bit_offset));
+        *(base + i) += ((*(aligned_ptr + i) & aligned_left) >> (bit_offset));
       }
 
       if(extra_byte()){
         *(base + sizeof(T)) = *(base + sizeof(T)) & right;
-        *(base + sizeof(T)) += ((*(copy_ptr + (sizeof(T)-1)) & copy_right) << (byte_size - bit_offset));
+        *(base + sizeof(T)) += ((*(aligned_ptr + (sizeof(T)-1)) & aligned_right) << (byte_size - bit_offset));
       }
     }
 
@@ -56,15 +56,15 @@ namespace optr
      *  Changes will only be written to the offsetted data after calling flush or by destroying the object.
      */
     void set(const T& value){
-      copy = value;
-      printf("%x\n", copy);
+      aligned = value;
+      printf("%x\n", aligned);
     }
 
     /**
-     * @brief Gets a copy of the object the offset_ptr points to.
+     * @brief Gets a aligned of the object the offset_ptr points to.
      */
     T get(){
-      return copy;
+      return aligned;
     }
   protected:
     uint8_t* base;
@@ -76,21 +76,21 @@ namespace optr
     size_t bit_offset;
     uint8_t left;
     uint8_t right;
-    T copy;
+    T aligned;
 
-    void make_copy(){
-      auto copy_ptr = reinterpret_cast<uint8_t * const>(&copy);
+    void make_aligned(){
+      auto aligned_ptr = reinterpret_cast<uint8_t * const>(&aligned);
       
       //Assumes that there actually is data to extract.
       static_assert(sizeof(T) > 0, "Cannot extract data from an empty data structure.");
 
       for(size_t i = 0; i < sizeof(T); i++){
         //Part that is already in the correct byte.
-        *(copy_ptr + i) = (*(base +i) & right) << bit_offset;
+        *(aligned_ptr + i) = (*(base +i) & right) << bit_offset;
         //Prevent access to byte after the last one in which the offsetted object is stored, when there is no offset.
         if(extra_byte()){
           //Part from the next byte that gets transferred.
-          *(copy_ptr + i) += (*(base + i + 1) & left) >> (byte_size - bit_offset);
+          *(aligned_ptr + i) += (*(base + i + 1) & left) >> (byte_size - bit_offset);
         }
       }
     }
